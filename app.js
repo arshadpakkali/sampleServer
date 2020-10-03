61f65242ef1d6eee8fab3e1820c4c870ca28677b
Arshad <arshadpakkali@gmail.com>
2020-10-03 12:30:29 +0530

    added a db connection

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var cors = require('cors')
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var cors = require("cors");
 var app = express();
 
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/demo", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", () => {
+  console.log("Error Connecting to DB");
+});
+
+db.once("open", () => {
+  console.log("Connected to The DB");
+});
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(cors())
-app.use(logger('dev'));
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -32,11 +46,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,31 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const mongoose = require("mongoose");
 
-/* GET home page. */
-router.get('/', function (req, res) {
-  res.render('index', {title: 'Express'});
+const randomSchema = new mongoose.Schema({
+  value: Number,
 });
 
-router.get('/data', (req, res) => {
-  res.json(5432)
-})
+const Random = mongoose.model("Random", randomSchema);
+
+/* GET home page. */
+router.get("/", function (req, res) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/data", async (req, res) => {
+  let data = await Random.find().select("-_id -__v");
+  res.json(data);
+});
+
+router.post("/add/:no", (req, res) => {
+  let data = req.params.no;
+
+  const item = new Random({ value: data });
+
+  item.save();
+
+  res.json("Data Saved in Db");
+});
 
 module.exports = router;
